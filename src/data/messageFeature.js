1f7ef04e755b71cc00dac3d5a27b73a39df1e0c6
nilishalu <shalini.c.e.ganu@gmail.com>
2023-08-12 20:01:52 +0530

    updated dummy data and modified css of messaging feature

--- src/data/messageFeature.js
+++ src/data/messageFeature.js
@@ -1,37 +1,37 @@
 export const messageFeature = [
    {
     title: "Easier Work Organization",
-    desc: "Efficiently unleash cross-media information without cross-media value. Quickly timely deliverables for real-time schemas. ",
+    desc: "Efficiently unleash cross-media information without cross-media value. Quickly timely deliverables for real-time schemas.",
     moreInfo: "www.google.com",
     logoLink: "https://cdn.pixabay.com/photo/2016/12/27/13/10/logo-1933884_640.png"
    },
    {
-      title: "Easier Work Organization",
-      desc: "Efficiently unleash cross-media information without cross-media value. Quickly timely deliverables for real-time schemas. ",
+      title: "Fast Connection",
+      desc: "Completely pursue scalable customer cross- media through potentialities. Holistically quickly installed portals.",
       moreInfo: "www.google.com",
       logoLink: "https://cdn.pixabay.com/photo/2016/12/27/13/10/logo-1933884_640.png"
    },
    {
-      title: "Easier Work Organization",
-      desc: "Efficiently unleash cross-media information without cross-media value. Quickly timely deliverables for real-time schemas. ",
+      title: "Streamlined Processes",
+      desc: "Objectively innovate empowered scalable manufactured products whereas parallel platforms predominate extensible.",
       moreInfo: "www.google.com",
       logoLink: "https://cdn.pixabay.com/photo/2016/12/27/13/10/logo-1933884_640.png"
    },
    {
-      title: "Easier Work Organization",
-      desc: "Efficiently unleash cross-media information without cross-media value. Quickly timely deliverables for real-time schemas. ",
+      title: "Easier Integrations",
+      desc: "Completely pursue scalable customer try through potentialities. Pontificate portals installed. Efficiently unleash information.",
       moreInfo: "www.google.com",
       logoLink: "https://cdn.pixabay.com/photo/2016/12/27/13/10/logo-1933884_640.png"
      },
      {
-      title: "Easier Work Organization",
-      desc: "Efficiently unleash cross-media information without cross-media value. Quickly timely deliverables for real-time schemas. ",
+      title: "Marketing Analytics",
+      desc: "Phosfluorescently engage worldwide methodologies with web-enabled  Interactively coordinate.",
       moreInfo: "www.google.com",
       logoLink: "https://cdn.pixabay.com/photo/2016/12/27/13/10/logo-1933884_640.png"
    },
    {
-      title: "Easier Work Organization",
-      desc: "Efficiently unleash cross-media information without cross-media value. Quickly timely deliverables for real-time schemas. ",
+      title: "Workflow Builder",
+      desc: "Collaboratively administrate turnkey service channels whereas virtual e-tailers. This  is objectively scalable metrics whereas.",
       moreInfo: "www.google.com",
       logoLink: "https://cdn.pixabay.com/photo/2016/12/27/13/10/logo-1933884_640.png"
      },
