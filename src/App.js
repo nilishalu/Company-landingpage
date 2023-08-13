7f26526d41b08b79e4fc8040c1da81863a83c41d
nilishalu <shalini.c.e.ganu@gmail.com>
2023-08-13 10:42:54 +0530

    restructured the files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
-import Home from './components/home/Home';
+import Home from './containers/home/Home';
 import Navbar from './components/navbar/Navbar';
-import FeatureMessage from './components/featureMessage/FeatureMessage';
+import FeatureMessage from './containers/featureMessage/FeatureMessage';
 
 function App() {
   return (
