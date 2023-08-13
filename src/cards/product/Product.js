05e478ffa646a2c7c5ba641636c7768f28b28d3d
nilishalu <shalini.c.e.ganu@gmail.com>
2023-08-13 19:59:43 +0530

    added dark theme product features

--- src/cards/product/Product.js
+++ src/cards/product/Product.js
@@ -2,15 +2,17 @@ import React from 'react';
 import "./product.css";
 
 const Product = (data) => {
-    const features = data.props;
+    const features = data.data;
+    const theme = data.theme;
+    
     return (
         <div className='product-features'>        
             {features.map((feature, index) => (
                 <div key={index} className='product-feature'>
                     <img src={feature.logoLink} alt='Logo' width={"60px"}></img>
                     <div className='info'>
-                    <h3 className='title'>{feature.title}</h3>
-                    <p className='desc'>{feature.desc}</p>
+                    <h3 className={theme === 'black' ? 'title-dark' : 'title-light'}>{feature.title}</h3>
+                    <p className={theme === 'black' ? 'desc-dark' : 'desc-light'}>{feature.desc}</p>
                     <a href={feature.moreInfo}>Learn more</a>
                     </div>
                 </div>
--- src/containers/productFeatures/ProductFeatures.js
+++ src/containers/productFeatures/ProductFeatures.js
@@ -4,15 +4,27 @@ import { productFeature } from '../../data/productFeature';
 import "./productFeature.css";
 
 const ProductFeatures = () => {
+
+    const darkTheme = 'black';
+    const lightTheme = 'white';
+ 
   return (
+    <>
     <div className='container'>
       <h2 className='center header'>Redefining Product Features</h2>
       <p className='center sub-header'>Keeping your eye on the ball while performing a deep dive on the start-up mentality to  derive convergence on cross-platform integration.</p>
        <div className='featuring'>
-       <Product props = {productFeature}/>
-       <Product props = {productFeature}/>
+       <Product data = {productFeature} theme = {lightTheme}/>
+       <Product data = {productFeature}/>
        </div>
     </div>
+    <div className='container page-second'>
+    <div className='featuring'>
+       <Product data = {productFeature}/>
+       <Product data = {productFeature} theme = {darkTheme}/>
+       </div>
+    </div>
+    </>
   )
 }
 
