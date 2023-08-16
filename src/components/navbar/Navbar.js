515641e16419796e8488f48e207883d7b19f9176
nilishalu <shalini.c.e.ganu@gmail.com>
2023-08-16 13:46:36 +0530

    make it responsive

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,18 +1,30 @@
-import React from 'react'
+import React from 'react';
+import { useState } from 'react';
 import "./navbar.css";
 
 const Navbar = () => {
+  const [navOpen, setNavOpen] = useState(false);
+
+  const toggleNav = () => {
+    setNavOpen(!navOpen);
+  };
+
   return (
-    <div className='navbar'>
-        <img src='/logo.png' className='logo' />
-        <a href=''>Home</a>
-        <a href=''>Our Products</a>
-        <a href=''>Resources</a>
-        <a href=''>Contacts</a>
-        <button className='login'>Login</button>
-        <button className='signup'>SignUp</button>
+    <div className="navbar">
+      <img src='/logo.png' className='logo' />
+      <button className={`navbar-toggle ${navOpen ? 'active' : ''}`} onClick={toggleNav}>
+        <span className="toggle-icon"></span>
+      </button>
+      <ul className={`nav-list ${navOpen ? 'active' : ''}`}>
+      <li><a href=''>Home</a></li>
+          <li><a href=''>Our Products</a></li>
+          <li><a href=''>Resources</a></li>
+          <li className='middle'><a href=''>Contacts</a></li>
+          <li><button className='login'>Login</button></li>
+          <li><button className='signup'>SignUp</button></li>
+      </ul>
     </div>
-  )
-}
+  );
+  };
 
 export default Navbar;
